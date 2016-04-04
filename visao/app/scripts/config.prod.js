d194b406fb4e98a256bf35cc9bad59e6cd7700ac
Vanderson <botelhojp@gmail.com>
2016-04-03 22:05:24 -0300

    dist

--- visao/app/scripts/config.prod.js
+++ visao/app/scripts/config.prod.js
@@ -3,9 +3,9 @@ angular.module('config', [])
 .constant
 (
 		'ENV', {
-			name:							'development',
-			oauth2_redirect:				'http://agentesdabolsa.com.br',
-			apiEndpoint:					'http://agentesdabolsa.com.br'
+			name:						'development',
+			oauth2_redirect:				'http://agentesdabolsa.com.br/s',
+			apiEndpoint:					'http://agentesdabolsa.com.br/s'
 		}
 )
 ;
