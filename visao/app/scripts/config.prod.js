32723fe5e060147ee2b2ee66000197bde8f95230
Vanderson <botelhojp@gmail.com>
2016-04-26 00:14:53 -0300

    url

--- visao/app/scripts/config.prod.js
+++ visao/app/scripts/config.prod.js
@@ -6,7 +6,7 @@ angular.module('config', [])
 			name:						'development',
 			oauth2_redirect:				'http://agentesdabolsa.com.br',
 			apiEndpoint:					'http://agentesdabolsa.com.br/s/agentesdabolsa',
-			wsEndpoint:					 	'ws://agentesdabolsa.com.br/s/agentesdabolsa/websocket'
+			wsEndpoint:					 	'ws://agentesdabolsa.com.br:8080/agentesdabolsa/websocket'
 		}
 )
 ;
